9d66defe1349b2425a47b191945e3a8d2f3c1f08
Rishi <rishi@malhotra.ca>
2019-01-13 01:55:15 -0500

    Roadmap added to main

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,5 +1,14 @@
 #include <ESP8266WiFi.h>
 
+/*
+ * Roadmap
+ * - setup http server
+ * - IR library
+ * - test local
+ * - Alexa and IFTTT
+ * - optimize (remove sleeps, better the code)
+ * - try on ESP01
+ */
 void setup() {
   // put your setup code here, to run once:
   wifi_setup();
